e05b355953117560133f649ed0d0e44b7d9e2749
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-06-04 16:58:08 +0700

    Update Handling Google OAUTH

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -54,7 +54,7 @@ exports.loginWithGoogleSupabase = async (req, res) => {
   try {
     const scope = process.env.NODE_ENV === 'production'
     ? 'https://frontend-tenangin.vercel.app/dashboard'
-    : 'http://localhost:5173/auth/callback';
+    : 'http://localhost:5173/#/auth/callback';
     
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
