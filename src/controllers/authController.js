e939e1fd6ccd9eef78859de8460125b7f564d274
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-06-04 17:26:49 +0700

    Update Google auth dengan scope auth callback url BE

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -24,7 +24,7 @@ exports.googleCallbackHandler = async (req, res) => {
 
     // Kirim token dan user info ke frontend (via redirect atau JSON)
     // Kalau redirect, bisa encode ke URL
-    const redirectUrl = `https://frontend-tenangin.vercel.app/#/auth/callback?token=${session.access_token}&id=${user.id}&username=${user.user_metadata.full_name || user.email}`;
+    const redirectUrl = `https://frontend-tenangin.vercel.app/#/auth/callback?access_token=${session.access_token}&id=${user.id}&username=${user.user_metadata.full_name || user.email}&email=${user.email}`;
 
     return res.redirect(redirectUrl);
 
@@ -52,14 +52,14 @@ exports.googleAuthSuccess = (req, res) => {
 // New login with Google using Supabase OAuth
 exports.loginWithGoogleSupabase = async (req, res) => {
   try {
-    const scope = process.env.NODE_ENV === 'production'
-    ? 'http://localhost:5173/google/callback'
-    : 'http://localhost:3000/#/google/callback';
-    // 
+    const redirectUrl = process.env.NODE_ENV === 'production'
+      ? 'https://tenangin-backend.vercel.app/auth/google/callback'
+      : 'http://localhost:5173/auth/callback';
+// https://tenangin-backend.vercel.app/
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: scope,
+        redirectTo: redirectUrl,
         queryParams: {
           access_type: 'offline',
           prompt: 'consent',
