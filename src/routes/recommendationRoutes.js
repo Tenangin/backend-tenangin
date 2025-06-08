199edf0f12b995a3c6179a3af1ae23bb2917eb96
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-06-08 09:37:21 +0700

    Update: API Recommendations

--- src/routes/recommendationRoutes.js
+++ src/routes/recommendationRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const recommendationController = require('../controllers/recommendationController');
 const { ensureAuthenticated } = require('../middlewares/auth');
-const jwtAuth = require('../middlewares/jwtAuth');
+const { jwtAuth } = require('../middlewares/auth');
 
 // router.use(ensureAuthenticated);
-router.use(jwtAuth);
+// router.use(jwtAuth);
 
 router.post('/add', jwtAuth, recommendationController.createRecommendation);
 router.get('/', jwtAuth, recommendationController.getRecommendations);
